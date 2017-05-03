32d85a9f66a6732c2b1fa2726642e1528e18a742
Vincent <vincentcent41@gmail.com>
2017-05-03 20:22:22 +0700

    Init django

--- inferno_backend/inferno_backend/urls.py
+++ inferno_backend/inferno_backend/urls.py
@@ -1,4 +1,4 @@
-"""inferno URL Configuration
+"""inferno_backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
--- inferno_backend/inferno_backend/wsgi.py
+++ inferno_backend/inferno_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for inferno project.
+WSGI config for inferno_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inferno.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inferno_backend.settings")
 
 application = get_wsgi_application()
